b3aaf2b9a16982559f10b3b0b5cc5f17a2a4839e
a123456789wwwqw <18210965650@163.com>
2021-08-18 13:39:27 +0800

    添加项目文件。

---
+++ GuCore/GuguguCore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using System.Drawing;
+
+namespace GuCore
+{
+    public interface IGuguguInterface
+    {
+        Window MainWindow();
+        string Name();
+        string Version();
+        Bitmap Icon();
+    }
+}
